667d9e1457955ccc9cac593ffb3c85994f823082
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-09-25 10:52:58 -0500

    Initial Commit

---
+++ MyMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMovies
+//
+//  Created by Fernando Caudillo Tafoya on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMovies/MyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesApp.swift
+//  MyMovies
+//
+//  Created by Fernando Caudillo Tafoya on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
